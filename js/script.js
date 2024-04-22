78ea71806d11d82d808493c7f2c46f900788c8dc
tristan5246339 <162559758+tristan5246339@users.noreply.github.com>
2024-04-22 13:08:31 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  var loginForm = document.getElementById('loginForm');
+  loginForm.onsubmit = function(event) {
+    event.preventDefault(); // 阻止表单默认提交行为
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // 在这里添加后端验证登录信息的代码
+  // 假设验证成功，展示成功页面；否则，展示失败页面
+  if (username === "admin" && password === "123456") {
+    window.location.href = "success.html";
+  } else {
+    window.location.href = "failure.html";
+	}
+  };
+};
